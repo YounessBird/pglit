16f6baf5c553e6602159aeae52bb7d4211bd5eda
YounessBird <67457600+YounessBird@users.noreply.github.com>
2022-03-31 02:14:37 +0100

    Update pglit.rs

--- tests/pglit.rs
+++ tests/pglit.rs
@@ -60,7 +60,7 @@ async fn createdb_and_dropdb_test() {
 
     // createdb test
     create_db(&mut config.clone(), "pgtools_db_test", NoTls, |res| {
-        assert!(res.is_ok());
+        //assert!(res.is_ok());
         eprintln!("database successfully created");
         if let Err(e) = res {
             eprintln!("{:?}", e.message);
@@ -70,7 +70,7 @@ async fn createdb_and_dropdb_test() {
 
     // Attempting to create a duplicate db
     create_db(&mut config.clone(), "pgtools_db_test", NoTls, |res| {
-        //assert!(res.is_err());
+        assert!(res.is_err());
         if let Err(e) = res {
             if e.code != "42P04" {
                 eprintln!("creating dublicate db should result a 42P04 error");
